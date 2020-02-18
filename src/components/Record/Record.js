8b24f9476f2e76523c7b38a937abc8f676c37e91
邢意龙 <xingyilong@aikucun.com>
2020-02-18 15:44:46 +0800

    黑胶唱片

---
+++ src/components/Record/Record.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Record.scss'
+function Record () {
+    return (
+        <div className="record">
+            <div className="borderOut">
+                <img src="./zjl/s.jpg"/>
+            </div>
+        </div>
+    )
+}
+
+export default Record;
--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import './Section.css';
-
+import './Section.scss';
+import Record  from '../Record/Record';
 function Section() {
   return (
     <div className="section">
-      Section 
+      <div className='bg'  style={{backgroundImage: "url('zjl/s.jpg')"}}></div>
+      <div className="section-content">
+        <Record></Record>
+      </div>
     </div>
   );
 }
